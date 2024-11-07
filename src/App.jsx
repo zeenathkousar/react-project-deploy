6ef7c1adbd3aa85a38153d3400c8f4a140156916
ZeenathKousar <zeenathzeenath2710@gmail.com>
2024-11-07 22:31:03 +0530

    added files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+
+  return (
+    <>
+    <h1>hello app</h1>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
